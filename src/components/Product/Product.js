18412f520e06aab8a59a79e643108c4a5d153a15
Michal Barczynski <barczynski_sggw@wp.pl>
2022-05-18 15:31:51 +0200

    add color, size choices & extraPrice for diff sizes

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,37 +1,54 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import shortid from 'shortid'; 
 
-const Product = props => {
+
+const Product = ({id, name, title, basePrice, colors, sizes}) => {
+
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0]);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
+  const getPrice = () => {
+    let extraPrice = sizes.find(elem => elem.name === currentSize.name);
+    return basePrice + extraPrice.additionalPrice;
+  }
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
         <img 
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt={`${title} shirt`}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>Kodilla shirt</h2>
-          <span className={styles.price}>Price: 20$</span>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {sizes.map((size, i) => 
+              <li key={shortid()}>
+                <button type="button" 
+                className={clsx(size, size === currentSize && styles.active)}
+                onClick={() => {
+                  setCurrentSize(sizes[i])}}>{size.name}</button></li>)}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {colors.map(color => <li key={shortid()}><button type="button" onClick= {() => {setCurrentColor(color)}} className={clsx(prepareColorClassName(color), currentColor === color && styles.active)}/></li>)}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,6 +58,14 @@ const Product = props => {
       </div>
     </article>
   )
+};
+
+Product.propTypes = {
+  name : PropTypes.string,
+  title: PropTypes.string,
+  basePrize: PropTypes.number,
+  colors: PropTypes.array,
+  sizes: PropTypes.array,
 };
 
 export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -7,7 +7,7 @@ const Products = () => {
 
   return (
     <section>
-      {products.map(product => <Product key={product.id} {...products}/>)}
+      {products.map(product => <Product key={product.id} {...product}/>)}
     </section>
   );
 };
